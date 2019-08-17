616df60282fc39a09c2c648c6b621d2aedb1ae32
PaulElcampeon <pauloladele@live.co.uk>
2019-08-17 15:24:07 +0100

    removed text in .properties file, added cross origin resource sharing

--- src/main/java/com/paulo/ticTacToe/controller/GameController.java
+++ src/main/java/com/paulo/ticTacToe/controller/GameController.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Controller
+@CrossOrigin("*")
 public class GameController {
 
     @Autowired
