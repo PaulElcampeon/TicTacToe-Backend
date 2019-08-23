43603861329d3c528594d85aa144badfbb6533a9
PaulElcampeon <pauloladele@live.co.uk>
2019-08-23 21:23:16 +0100

    attained build folder from react app and now serving static content from this server

---
+++ src/main/resources/static/precache-manifest.35336df52d8be68bf091726c287a7c73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791afda199eac25874a4e84c2278ccaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f55ef975dcfee70767d",
+    "url": "/static/css/main.5df81bd5.chunk.css"
+  },
+  {
+    "revision": "24bb1de0c4d616cc7891",
+    "url": "/static/js/2.bad4ac01.chunk.js"
+  },
+  {
+    "revision": "3f55ef975dcfee70767d",
+    "url": "/static/js/main.df0bbdb6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
